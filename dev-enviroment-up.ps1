65c18c5d1ae13078ec04c3d76143c61960b9b567
Javier <139801409+JamerWolf@users.noreply.github.com>
2025-08-25 16:32:12 -0500

    activate wsl integration in distro "Ubuntu-22.04"

--- dev-enviroment-up.ps1
+++ dev-enviroment-up.ps1
@@ -1,5 +1,5 @@
-#TODO: agregar funcionalidad de ejecutar una linea especifica por medio del StateFile
-# Verify that the script is running as an administrator
+#TODO: agregar funcionalidad de ejecutar desde una linea especifica por medio del StateFile
+
 function relaunch_admin_script {
     # Relaunch script with administrator privileges
     Start-Process powershell -ArgumentList `
@@ -18,35 +18,34 @@ if (-not $IsAdmin) {
 }
 
 $ScriptPath = $MyInvocation.MyCommand.Path
+# Persistent flag to know which step to execute
 $StateFile = "$env:TEMP\script_state.txt"
 
 $cases = [ordered]@{
     "start" = {
         # Enable necessary features
-        Write-Host "Start"
         dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
-        #dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+        dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
   
         $Command = "powershell.exe -NoProfile -ExecutionPolicy Bypass -File `"$ScriptPath`""
 
         # Add to RunOnce in the registry
-        #Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce" `
-        #    -Name "MiScriptRunOnce" -Value $Command
+        Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce" `
+            -Name "MiScriptRunOnce" -Value $Command
 
-         "wsl_setup" | Out-File $StateFile -Encoding UTF8
+        "wsl_setup" | Out-File $StateFile -Encoding UTF8
 
         Write-Host "El sistema se va a reiniciar. presione cualquier tecla para continuar..."
         [void][System.Console]::ReadKey($true)
 
-        #Restart-Computer -Force
+        Restart-Computer -Force
     }
 
     "wsl_setup"  = {
-        Write-Host "wsl_setup"
         wsl --install --no-distribution
 
-        Invoke-WebRequest -Uri https://cloud-images.ubuntu.com/wsl/releases/22.04/current/ubuntu-jammy-wsl-amd64-wsl.rootfs.tar.gz `
-            -OutFile $env:TEMP\ubuntu-jammy-wsl-amd64-wsl.rootfs.tar.gz
+        Start-BitsTransfer -Source https://cloud-images.ubuntu.com/wsl/releases/22.04/current/ubuntu-jammy-wsl-amd64-wsl.rootfs.tar.gz `
+            -Destination $env:TEMP\ubuntu-jammy-wsl-amd64-wsl.rootfs.tar.gz
 
         New-Item -ItemType Directory -Force -Path C:\WSL\Ubuntu2204\
 
@@ -70,7 +69,42 @@ $cases = [ordered]@{
 
         Start-Process -FilePath "$env:TEMP\Docker%20Desktop%20Installer.exe" -ArgumentList "install", "--quiet" -Wait
 
-        docker version
+        docker desktop update -q
+
+        docker -v
+
+        "conf_wsl_integration" | Out-File $StateFile -Encoding UTF8
+        
+    }
+
+    "conf_docker_wsl_integration" = {
+        $settingsPath = "$env:APPDATA\Docker\settings-store.json"
+        
+        if (-Not (Test-Path $settingsPath)) {
+            Write-Host "No se encontró Docker Desktop instalado o el archivo settings.json."
+            #exit 1
+        }
+
+        $json = Get-Content $settingsPath -Raw | ConvertFrom-Json
+
+        if (-not $json.IntegratedWslDistros) {
+            $json | Add-Member -MemberType NoteProperty -Name IntegratedWslDistros -Value $null
+        }
+
+        if (-not ($json.IntegratedWslDistros -contains "Ubuntu-22.04")) {
+            $json.IntegratedWslDistros = @()
+            $json.IntegratedWslDistros += "Ubuntu-22.04"
+        }
+
+        $jsonString = $json | ConvertTo-Json -Depth 10
+
+        [System.IO.File]::WriteAllText(
+            $settingsPath,
+            $jsonString,
+            (New-Object System.Text.UTF8Encoding($false))
+        )
+
+        docker desktop restart
 
         "clean" | Out-File $StateFile -Encoding UTF8
     }
@@ -80,7 +114,7 @@ $cases = [ordered]@{
         "$env:TEMP\ubuntu-jammy-wsl-amd64-wsl.rootfs.tar.gz", `
         "$env:TEMP\wsl.2.5.10.0.x64.msi", `
         "$env:TEMP\Docker%20Desktop%20Installer.exe" `
-        #"$env:TEMP\script_state.txt" `
+        "$env:TEMP\script_state.txt" `
         -Force -ErrorAction SilentlyContinue
 
         exit
